706d6921aa6da097235a51e587dd48f72937e469
AileOrgla <aileorgla@gmail.com>
2014-09-30 12:37:00 +0300

    Add ül3 -need some improvements

--- layouts/team.php
+++ layouts/team.php
@@ -10,6 +10,13 @@
 
     <main id="site-body">
       <section id="site-content">
+          <?php
+            $member_name=array('Kaspar Naaber', 'Peeter Kartul', 'Erki Nool', 'Timo Tigu', 'Silver Kuu', 'Tauno Päike');
+            $e_mail=array('kaspar.naaber@khk.ee', 'peeter.kartul@khk.ee', 'erki.nool@khk.ee', 'timo.tigu@khk.ee', 'silver.kuu@khk.ee', 'tauno.paike@khk.ee');
+            $intrest=array('Arvutid', 'Jalgrattad', 'Jalgpall', 'Raamatud', 'Peod', 'Kalastamine');
+
+          foreach
+          ?>
         <div class="team-member">
           <h2 class="member-name">Kaspar Naaber</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:kaspar.naaber@khk.ee">kaspar.naaber@khk.ee</a></div>
@@ -19,31 +26,31 @@
         <div class="team-member">
           <h2 class="member-name">Peeter Kartul</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:peeter.kartul@khk.ee">peeter.kartul@khk.ee</a></div>
-          <div class="member-info"><span>Amet: </span>Jalgrattad</div>
+          <div class="member-info"><span>Huvialad: </span>Jalgrattad</div>
         </div>
 
         <div class="team-member">
           <h2 class="member-name">Erki Nool</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:erki.nool@khk.ee">erki.nool@khk.ee</a></div>
-          <div class="member-info"><span>Amet: </span>Jalgpall</div>
+          <div class="member-info"><span>Huvialad: </span>Jalgpall</div>
         </div>
 
         <div class="team-member">
           <h2 class="member-name">Timo Tigu</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:timo.tigu@khk.ee">timo.tigu@khk.ee</a></div>
-          <div class="member-info"><span>Amet: </span>Raamatud</div>
+          <div class="member-info"><span>Huvialad: </span>Raamatud</div>
         </div>
 
         <div class="team-member">
           <h2 class="member-name">Silver Kuu</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:silver.kuu@khk.ee">silver.kuu@khk.ee</a></div>
-          <div class="member-info"><span>Amet: </span>Peod</div>
+          <div class="member-info"><span>Huvialad: </span>Peod</div>
         </div>
 
         <div class="team-member">
           <h2 class="member-name">Tauno Päike</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:tauno.paike@khk.ee">tauno.paike@khk.ee</a></div>
-          <div class="member-info"><span>Amet: </span>Kalastamine</div>
+          <div class="member-info"><span>Huvialad: </span>Kalastamine</div>
         </div>
       </section>
     </main>
