4bc1a0d32e7521f663cdd05010381a80e9ba6bc1
AileOrgla <aileorgla@gmail.com>
2014-09-30 11:54:47 +0300

    Add ül2

--- components/site-data.php
+++ components/site-data.php
@@ -12,21 +12,21 @@
   $blog_news =
     array(
       array(
-        "post_title" => "Eesti Disainiauhindade pidulik gala tõi ohtralt auhindu ja moeka seltskonna",
-        "post_excerpt" => "<p>Reedel, 26. septembril toimus Lilleküla jalgpallihallis pidulik Eesti Disainiauhinnad 2014 pidulik gala, kus pärjati parimaid kodumaiseid disainereid. Selle aasta parimad disainerid tunnustati viies kategoorias: tootedisain, graafiline disain, veebidisain, noor disainer ja teenusedisain. Silmapaistvaimad tegijad valiti välja 434 erineva töö seast.</p>",
-        "post_body" => "<p>Graafilise disaini üheks laureaadiks said disainerid Kerli Virk ja Stella Skulatšjova, kataloogiga Kaamos, mis on mahukaim ülevaade tänapäeva Eesti moeväljast. Veel sai tunnustust Kalevi Eesti laulu-ja tantsupeo šokolaadipakend, mille kujundas pakendidisainile orienteeritud firma KOOR.</p><p>Noore disaineri auhinna SÄSI pälvis Aap Piho, kes on nüüdseks suutnud oma brändiga Warm North ilma teha ka rahvusvahelisel turul. “SÄSI preemia esikümme kujunes sel aastal väga tasavägiseks, sest pea kõik disaineritel omavad enda brändi ning reaalset ettevõtluskogemust,” rääkis žürii esimees, disainer Martin Pärn. Väikse SÄSI preemia määrati Mariti Ilisonile, kes on kollektsiooniga “Magada tahaks” andnud käsitööle ja lookaalsusele täiesti uue mõõtme ning sidunud sellega taaskasutuse ja säästliku tootmise mõõtmed.”</p><p>Tööstusdisaini auhinna pälvis disainer Mihkel Güssoni loodud surfarite abimees Shaka Weather Station, mida saavad kasutada purjetajad, lohesurfarid, mägironijad. Shaka puhul hindas žürii toote uuenduslikkust, innovaatilisust ja ülimat praktilisust. Parima autoridisaini auhind läks Tarmo Luisu Riidepuu-lambile What?, mis on tõeliselt universaalne ja üllatuslik valgusti. </p>"
+        "post-title" => "Eesti Disainiauhindade pidulik gala tõi ohtralt auhindu ja moeka seltskonna",
+        "post-excerpt" => "<p>Reedel, 26. septembril toimus Lilleküla jalgpallihallis pidulik Eesti Disainiauhinnad 2014 pidulik gala, kus pärjati parimaid kodumaiseid disainereid. Selle aasta parimad disainerid tunnustati viies kategoorias: tootedisain, graafiline disain, veebidisain, noor disainer ja teenusedisain. Silmapaistvaimad tegijad valiti välja 434 erineva töö seast.</p>",
+        "post-body" => "<p>Graafilise disaini üheks laureaadiks said disainerid Kerli Virk ja Stella Skulatšjova, kataloogiga Kaamos, mis on mahukaim ülevaade tänapäeva Eesti moeväljast. Veel sai tunnustust Kalevi Eesti laulu-ja tantsupeo šokolaadipakend, mille kujundas pakendidisainile orienteeritud firma KOOR.</p><p>Noore disaineri auhinna SÄSI pälvis Aap Piho, kes on nüüdseks suutnud oma brändiga Warm North ilma teha ka rahvusvahelisel turul. “SÄSI preemia esikümme kujunes sel aastal väga tasavägiseks, sest pea kõik disaineritel omavad enda brändi ning reaalset ettevõtluskogemust,” rääkis žürii esimees, disainer Martin Pärn. Väikse SÄSI preemia määrati Mariti Ilisonile, kes on kollektsiooniga “Magada tahaks” andnud käsitööle ja lookaalsusele täiesti uue mõõtme ning sidunud sellega taaskasutuse ja säästliku tootmise mõõtmed.”</p><p>Tööstusdisaini auhinna pälvis disainer Mihkel Güssoni loodud surfarite abimees Shaka Weather Station, mida saavad kasutada purjetajad, lohesurfarid, mägironijad. Shaka puhul hindas žürii toote uuenduslikkust, innovaatilisust ja ülimat praktilisust. Parima autoridisaini auhind läks Tarmo Luisu Riidepuu-lambile What?, mis on tõeliselt universaalne ja üllatuslik valgusti. </p>"
       ),
 
       array(
-        "post_title" => "Snooki sünnitas tütre",
-        "post_excerpt" => "<p>Tõsielusarja «Jersey Shore» täht Nicole «Snooki» Polizzi ja tema kihlatu Jionni LaValle said teise lapse.</p>",
-        "post_body" => "<p>Staar kinnitas, et pisitütar Giovanna Marie LaValle sündis 26. septembril kell 4:28 hommikul, vahendab Us Weekly.</p><p>26-aastase Snooki peres kasvab ka 2012. aasta augustis sündinud poeg Lorenzo. Snooki teatas uudisest Twitteri vahendusel:</p>"
+        "post-title" => "Snooki sünnitas tütre",
+        "post-excerpt" => "<p>Tõsielusarja «Jersey Shore» täht Nicole «Snooki» Polizzi ja tema kihlatu Jionni LaValle said teise lapse.</p>",
+        "post-body" => "<p>Staar kinnitas, et pisitütar Giovanna Marie LaValle sündis 26. septembril kell 4:28 hommikul, vahendab Us Weekly.</p><p>26-aastase Snooki peres kasvab ka 2012. aasta augustis sündinud poeg Lorenzo. Snooki teatas uudisest Twitteri vahendusel:</p>"
       ),
 
       array(
-        "post_title" => "Pisike North West sammus Pariisis vapralt ema kõrval",
-        "post_excerpt" => "<p>Kim Kardashian ja Kanye West saabusid Pariisi moenädalale koos 15 kuu vanuse tütre North Westiga.</p>",
-        "post_body" => "<p>«Hotellist lahkudes hakkas ta nutma ja me ei saanud teda maha jätta. See oli viimase hetke otsus,» põhjendas Kim, miks ta lapse üritusele kaasa võttis.</p>"
+        "post-title" => "Pisike North West sammus Pariisis vapralt ema kõrval",
+        "post-excerpt" => "<p>Kim Kardashian ja Kanye West saabusid Pariisi moenädalale koos 15 kuu vanuse tütre North Westiga.</p>",
+        "post-body" => "<p>«Hotellist lahkudes hakkas ta nutma ja me ei saanud teda maha jätta. See oli viimase hetke otsus,» põhjendas Kim, miks ta lapse üritusele kaasa võttis.</p>"
       )
     )
   ;
